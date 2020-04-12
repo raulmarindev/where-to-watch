7969aa2db610872d65c60c7106c0fa72c11c4aae
Raúl Marín <rmarin82@gmail.com>
2020-04-12 20:28:03 +0200

    Add eslint with Airbnb configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "import/no-unresolved": ["off"],
+    "import/order": "off",
+    "react/jsx-filename-extension": "off",
+    "import/extensions": "off",
+    "no-unused-vars": "off",
+    "linebreak-style": ["error", "unix"],
+  },
+};
