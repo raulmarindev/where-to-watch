77c51ac31c62f05e3c51d5bd6f315c962bec04a1
Raúl Marín <rmarin82@gmail.com>
2020-04-15 13:23:20 +0200

    Add parsing of json fields which have a different name from the ITitle field.

--- src/api/agents/titles.ts
+++ src/api/agents/titles.ts
@@ -10,10 +10,29 @@ const requests = {
   get: (url: string) => http.get(url).then(responseBody),
 };
 
+function getParsedResults(response: any) {
+  const parsedResponse: IFilterResponse = response;
+  const { results: parsedResults } = parsedResponse;
+  const { results } = response;
+
+  for (let i = 0; i < parsedResults.length; i += 1) {
+    for (let j = 0; j < parsedResults[i].locations.length; j += 1) {
+      parsedResults[i].locations[j].displayName = results[i].locations[j].display_name;
+    }
+  }
+
+  return parsedResults;
+}
+
 const Titles = {
   filter: async (searchTerm?: string, countryCode: string = 'es'): Promise<ITitle[]> => {
-    const response: IFilterResponse = await requests.get(`/lookup?term=${searchTerm}&country=${countryCode}`);
-    return response.results;
+    const response = await requests.get(`/lookup?term=${searchTerm}&country=${countryCode}`);
+
+    if (response) {
+      return getParsedResults(response);
+    }
+
+    return [];
   },
   find: async (titleId: string, source: string = 'imdb', countryCode: string = 'es'): Promise<ITitle> => {
     const response: IFindResponse = await requests.get(`/idlookup?source_id=${titleId}&source=${source}&country=${countryCode}`);
