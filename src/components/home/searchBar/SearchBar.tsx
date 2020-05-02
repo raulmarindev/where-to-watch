d5a4f6908aa6772f7eb60ce51c33597d6951de8e
Raúl Marín <rmarin82@gmail.com>
2020-05-02 23:35:48 +0200

    Improve SearchBar responsiveness.

--- src/components/home/searchBar/SearchBar.tsx
+++ src/components/home/searchBar/SearchBar.tsx
@@ -9,18 +9,15 @@ interface ISearchBarProps {
 
 const SearchBar: React.FC<ISearchBarProps> = ({ value, onChange, placeHolder }) => (
   <Row>
-    <Col sm={0} md={1} />
-    <Col sm={12} md={10}>
+    <Col xs={0} md={1} />
+    <Col xs={12} md={10}>
       <Form className="px-4" onSubmit={(event: React.FormEvent<HTMLFormElement>) => { event.preventDefault(); }}>
         <Form.Group controlId="searchTerm">
           <Form.Control className="mb-3" size="lg" type="search" value={value} onChange={onChange} placeholder={placeHolder} />
-          <Form.Text className="text-muted w-25 mx-auto">
-            Enter at least 3 characters to start the search
-          </Form.Text>
         </Form.Group>
       </Form>
     </Col>
-    <Col sm={0} md={1} />
+    <Col xs={0} md={1} />
   </Row>
 );
 
--- src/components/home/searchHeader/SearchHeader.tsx
+++ src/components/home/searchHeader/SearchHeader.tsx
@@ -10,23 +10,23 @@ font-family: 'Fjalla One';
 const Header: React.FC<{ className?: string; }> = ({ className }) => (
   <>
     <Row className={className}>
-      <Col sm={0} md={5} />
-      <Col sm={12} md={2}>
-        <h2 className="w-75 mx-auto">
+      <Col xs={3} md={4} lg={5} />
+      <Col xs={6} md={4} lg={2}>
+        <h2>
           <SearchTitle className="w-100 mx-auto d-block">
             <MagnifyingGlassIcon height={32} width={32} />
             {'   Search'}
           </SearchTitle>
         </h2>
       </Col>
-      <Col sm={0} md={5} />
+      <Col xs={3} md={4} lg={5} />
     </Row>
     <Row className={className}>
-      <Col sm={0} md={4} />
-      <Col sm={12} md={4}>
+      <Col sm={0} md={2} lg={4} />
+      <Col sm={12} md={8} lg={4}>
         <h5 className="text-black-50 w-75 mx-auto">Search the best movies and tv shows</h5>
       </Col>
-      <Col sm={0} md={4} />
+      <Col sm={0} md={2} lg={4} />
     </Row>
   </>
 );
