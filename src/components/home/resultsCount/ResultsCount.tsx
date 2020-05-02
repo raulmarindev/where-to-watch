6dcd11865e4754b23c94d30325ff5b35c4b7b4c6
Raúl Marín <rmarin82@gmail.com>
2020-05-02 23:35:23 +0200

    Improve results component styling.

--- src/components/home/resultsCount/ResultsCount.tsx
+++ src/components/home/resultsCount/ResultsCount.tsx
@@ -1,13 +1,19 @@
 import { ListGroup } from 'imports/bootstrap';
 import React from 'react';
+import styled from 'styled-components';
+
+const Span = styled.span`
+width: 75px
+`;
 
 const ResultsCount: React.FC<{ resultsQuantity: number; }> = ({ resultsQuantity }) => (
   <ListGroup.Item className="px-md-2 px-0 border-top">
-    <span className="d-block mx-auto">
+    <Span className="d-block mx-auto">
       {resultsQuantity}
       {' '}
-      results
-    </span>
+      result
+      {resultsQuantity > 1 ? 's' : ''}
+    </Span>
   </ListGroup.Item>
 );
 
