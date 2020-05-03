5c451b4109d8aebc48824bfad295e57242473981
Raúl Marín <rmarin82@gmail.com>
2020-05-03 13:26:53 +0200

    Add sass to the project.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from 'components/app/App';
 import { createBrowserHistory } from 'history';
-import 'index.css';
+import 'index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
