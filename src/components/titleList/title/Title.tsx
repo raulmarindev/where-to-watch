310f78744197b9b6dcea1a2f0774b1a8e645ff03
Raúl Marín <rmarin82@gmail.com>
2020-05-11 21:10:24 +0200

    Add lazy loading for the images.

--- src/components/titleList/title/Title.tsx
+++ src/components/titleList/title/Title.tsx
@@ -1,6 +1,9 @@
 import ITitle from 'api/models/ITitle';
 import LocationList from 'components/locationList/LocationList';
+import Loading from 'images/Loading.gif';
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import styled from 'styled-components';
 import {
   Col, Container, ListGroup, Row, Card,
 } from 'imports/bootstrap';
@@ -8,6 +11,10 @@ import {
 interface ITitleProps {
   title: ITitle;
 }
+
+const LazyLoadImageWithStyles = styled(LazyLoadImage)`
+max-height: 200px;
+`;
 
 const Title: React.FC<ITitleProps> = ({ title }) => {
   const { locations, name, picture } = title;
@@ -20,7 +27,13 @@ const Title: React.FC<ITitleProps> = ({ title }) => {
           <Col md={3} />
           <Col md={3}>
             <Card className="bg-dark text-white">
-              <Card.Img src={picture} alt={name} />
+              <LazyLoadImageWithStyles
+                loading="lazy"
+                className="card-img"
+                alt={name}
+                src={picture}
+                placeholderSrc={Loading}
+              />
               <Card.ImgOverlay>
                 <Card.Text className="h2 outlined">{titleText}</Card.Text>
               </Card.ImgOverlay>
