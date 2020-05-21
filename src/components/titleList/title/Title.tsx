324967f9595aa06ad18057932c0d817219d99f4f
Raúl Marín <rmarin82@gmail.com>
2020-05-21 23:19:53 +0200

    Improve responsiveness of the Title component.

--- src/components/titleList/title/Title.tsx
+++ src/components/titleList/title/Title.tsx
@@ -20,29 +20,32 @@ const Title: React.FC<ITitleProps> = ({ title }) => {
   const { locations, name, picture } = title;
   const characterLimit = 60;
   const titleText = name.length < characterLimit ? name : `${name.substring(0, characterLimit)}...`;
+  const imageWidth = 350;
+  const imageSrcSet = `${picture}?w=${imageWidth} 1x,${picture}?w=${imageWidth}&dpr=2 2x,${picture}?w=${imageWidth}&dpr=3 3x,${picture}?w=${imageWidth}&dpr=4 4x`;
   return (
     <ListGroup.Item className="px-md-2 px-0">
       <Container fluid>
         <Row>
-          <Col md={3} />
-          <Col md={3}>
+          <Col md={1} lg={3} />
+          <Col md={5} lg={3} sm={8} xs={12}>
             <Card className="bg-dark text-white">
               <LazyLoadImageWithStyles
                 loading="lazy"
                 className="card-img"
                 alt={name}
-                src={picture}
+                src={`${picture}?w=${imageWidth}`}
                 placeholderSrc={Loading}
+                srcSet={imageSrcSet}
               />
               <Card.ImgOverlay>
                 <Card.Text className="h2 outlined">{titleText}</Card.Text>
               </Card.ImgOverlay>
             </Card>
           </Col>
-          <Col sm={8} md={3}>
+          <Col sm={8} md={6} lg={3}>
             <LocationList locations={locations} />
           </Col>
-          <Col sm={0} md={3} />
+          <Col sm={0} md={0} lg={3} />
         </Row>
       </Container>
     </ListGroup.Item>
