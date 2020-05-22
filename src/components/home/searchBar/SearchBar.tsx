846240b21dd52048b0ee4a63dc25117baf425e78
Raúl Marín <rmarin82@gmail.com>
2020-05-22 23:45:08 +0200

    Remove unused styles from the SearchBar component.

--- src/components/home/searchBar/SearchBar.tsx
+++ src/components/home/searchBar/SearchBar.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import {
   Form, Container,
 } from 'imports/bootstrap';
-
-const FormControlWithStyles = styled(Form.Control)`
-  -webkit-appearance: searchfield-cancel-button;
-`;
 
 interface ISearchBarProps {
   value: string;
@@ -18,7 +13,7 @@ const SearchBar: React.FC<ISearchBarProps> = ({ value, onChange, placeHolder }) 
   <Container className="px-0">
     <Form onSubmit={(event: React.FormEvent<HTMLFormElement>) => { event.preventDefault(); }}>
       <Form.Group className="mb-0" controlId="searchTerm">
-        <FormControlWithStyles size="lg" type="search" value={value} onChange={onChange} placeholder={placeHolder} />
+        <Form.Control size="lg" type="search" value={value} onChange={onChange} placeholder={placeHolder} />
       </Form.Group>
     </Form>
   </Container>
