5fc6ed2ac636c70c4a3b014ca42b33deeeb0179f
Raúl Marín <rmarin82@gmail.com>
2020-05-26 20:26:03 +0200

    Update not found page.

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,16 +1,11 @@
-import Error404 from 'images/Error404.jpg';
-import { Image } from 'imports/bootstrap';
+import Error404 from 'images/Error404.svg';
+import { Container } from 'imports/bootstrap';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const NotFound: React.FC = () => (
-  <>
-    <Image src={Error404} fluid />
-    <a href="https://www.freepik.com/free-photos-vectors/business">Business vector created by pikisuperstar - www.freepik.com</a>
-    <p style={{ textAlign: 'center' }}>
-      <Link to="/">Go to Home </Link>
-    </p>
-  </>
+  <Container className="w-xl-50">
+    <img src={Error404} alt="Error404" />
+  </Container>
 );
 
 
